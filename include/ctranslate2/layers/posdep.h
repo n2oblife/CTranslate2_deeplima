fd56efed4f6e6dbf31294e120653ecb503d6452c
Gael de Chalendar <gael.de-chalendar@cea.fr>
2023-09-03 23:17:23 +0200

    posdep.cc nearly compiles

--- include/ctranslate2/layers/posdep.h
+++ include/ctranslate2/layers/posdep.h
@@ -8,117 +8,112 @@
 #include "ctranslate2/ops/activation.h"
 #include "ctranslate2/padder.h"
 
-namespace ctranslate2 {
-    namespace layers {
-        
-        class DeepBiaffine : public Layer
-        {
-            public:
-            DeepBiaffine(
-                const models::Model& model,
-                const std::string& scope,
-                const ops::ActivationType activation_type = ops::ActivationType::ReLU
-            );
+namespace ctranslate2::layers {
 
-            ~DeepBiaffine();
+class DeepBiaffine : public Layer
+{
+public:
+  DeepBiaffine(
+      const models::Model& model,
+      const std::string& scope,
+      const int in_dim1,
+      const int in_dim2,
+      const int output_dim,
+      const ops::ActivationType activation_type = ops::ActivationType::ReLU
+  );
 
-            void operator()(
-                const StorageView& x1,
-                const StorageView& x2,
-                StorageView& output
-            );
+  ~DeepBiaffine() = default;
 
-            void make_interactions(
-                const StorageView& h,
-                StorageView& g
-            );
+  void operator()(
+      const StorageView& x1,
+      const StorageView& x2,
+      StorageView& output
+  ) const;
 
-            StorageView pairwise() const{
-                return _pairwise_weight.view(-1, (_in_dim2+1)*_output_dim);
-            }
+  void make_interactions(const StorageView& h,
+                          StorageView& g) const;
 
-            private:
-            const ops::ActivationType _activation;
-            const std::unique_ptr<const Dense> _ffn1;
-            const std::unique_ptr<const Dense> _ffn2;
-            const StorageView _pairwise_weight;
-            ops::Concat _cat;
-            const ops::MatMul _matmul;
-            const dim_t _in_dim1;
-            const dim_t _in_dim2;
-            const dim_t _output_dim;
+  StorageView pairwise() const;
 
-        };
+private:
+  const ops::ActivationType _activation;
+  const std::unique_ptr<const Dense> _ffn1;
+  const std::unique_ptr<const Dense> _ffn2;
+  const StorageView _pairwise_weight;
+  ops::Concat _cat;
+  const ops::MatMul _matmul;
+  const dim_t _in_dim1;
+  const dim_t _in_dim2;
+  const dim_t _output_dim;
+};
 
-        class PosdepDecoder : public Decoder // or Layer
-        {
-            public:
-            PosdepDecoder(
-                const models::Model& model, 
-                const std::string& scope,
-                const ops::ActivationType* activation_type = ops::ActivationType::ReLU,
-            );
+class PosdepDecoder : public Decoder // or Layer
+{
+public:
+  PosdepDecoder(
+      const models::Model& model,
+      const std::string& scope,
+      const ops::ActivationType activation_type = ops::ActivationType::ReLU
+  );
 
-            ~PosdepDecoder();
+  ~PosdepDecoder() = default;
 
-            void operator()( // add the parameters needed for feedforward
-                const StorageView& batch,
-                const StorageView& word_reprs,
-                const StorageView& cls_reprs,
-                StorageView& preds_output,
-                StorageView& deps_idxs_output
-            ) const;
+  void operator()( // add the parameters needed for feedforward
+      const StorageView& batch,
+      const StorageView& word_reprs,
+      const StorageView& cls_reprs,
+      StorageView& preds_output,
+      StorageView& deps_idxs_output
+  ) const;
 
-            void predict(
-                const StorageView batch, 
-                const StorageView word_reprs, 
-                const StorageView cls_reprs,
-                StorageView& predicted_upos, 
-                StorageView& predicted_xpos, 
-                StorageView& predicted_feats, 
-                StorageView& dep_preds
-            ); 
+  void predict(
+      const StorageView batch,
+      const StorageView word_reprs,
+      const StorageView cls_reprs,
+      StorageView& predicted_upos,
+      StorageView& predicted_xpos,
+      StorageView& predicted_feats,
+      StorageView& dep_preds
+  ) const;
 
-            void preds_to_cpu(const StorageView& prediction);
+  void preds_to_cpu(const StorageView& prediction);
 
-            void deprel_to_deps(
-                const StorageView& predicted_dep,
-                const int& batch_size,
-                StorageView& output
-            );
+  void deprel_to_deps(
+      const StorageView& predicted_dep,
+      const int& batch_size,
+      StorageView& output
+  );
 
-            void padding_deps(
-                const StorageView& dep,
-                StorageView& padded_dep
-            );
+  void padding_deps(
+      const StorageView& dep,
+      StorageView& padded_dep
+  );
 
 
-            private:
-            const Embeddings _upos_embeddings;
-            const std::unique_ptr<const Dense> _upos_ffn;
-            const std::unique_ptr<const Dense> _xpos_ffn;
-            const std::unique_ptr<const Dense> _feats_ffn;
-            const std::unique_ptr<const Dense> _down_project;
-            const std::unique_ptr<const DeepBiaffine> _unlabeled;
-            const std::unique_ptr<const DeepBiaffine> _deprel;
-        };
+private:
+  const Embeddings _upos_embeddings;
+  const std::unique_ptr<const Dense> _upos_ffn;
+  const std::unique_ptr<const Dense> _xpos_ffn;
+  const std::unique_ptr<const Dense> _feats_ffn;
+  const std::unique_ptr<const Dense> _down_project;
+  const std::unique_ptr<const DeepBiaffine> _unlabeled;
+  const std::unique_ptr<const DeepBiaffine> _deprel;
+};
 
-        class DeepLayer : public Layer 
-        {
-            DeepLayer(
-                const models::Model& model,
-                const std::string& scope,
+class DeepLayer : public Layer
+{
+public:
+  DeepLayer(
+    const models::Model& model,
+    const std::string& scope
+  );
 
-            );
+  void operator()(
 
-            void operator()(
+  );
 
-            );
+private: // build dynamic parameter
+};
 
-            private: // build dynamic parameter
-        };
+} // namespace ctranslate2::layers
 
-
-    } // namespace layers
-    
-} // namespace ctranslate2
--- src/layers/posdep.cc
+++ src/layers/posdep.cc
@@ -1,155 +1,128 @@
 #include "ctranslate2/layers/posdep.h"
+#include "ctranslate2/ops/ops.h"
 
 #include <cmath>
 
-namespace ctranslate2 {
-    namespace layers {
+namespace ctranslate2::layers
+{
 
-        /**
-         * This class is based on the implmentation from trankit
-         * from the work of Dozat & Manning 
-        */
-        void DeepBiaffine(
-            const models::Model& model,
-            const std::string& scope,
-            const ops::ActivationType activation_type,
-            const int in_dim1,
-            const int in_dim2,
-            const int output_dim
-        ) : _activation(activation_type),
-        // TODO ensure the class of the ffn
-        //TODO revoir cette classe pour la recup des poids
-         _ffn1(),
-         _ffn2(),
-         _pairwise_weight(), 
-         _in_dim1(in_dim1),
-         _in_dim2(in_dim2),
-         _output_dim(output_dim),
-         _cat(0),
-         _matmul()
-        {}
+/**
+* This class is based on the implementation from trankit
+* from the work of Dozat & Manning
+*/
+DeepBiaffine::DeepBiaffine(const models::Model& model,
+                           const std::string& scope,
+                           const int in_dim1,
+                           const int in_dim2,
+                           const int output_dim,
+                           const ops::ActivationType activation_type
+                           ) :
+    _activation(activation_type),
+    // TODO ensure the class of the ffn
+    // TODO review this class for weighs retrieval
+    _ffn1(),
+    _ffn2(),
+    _pairwise_weight(),
+    _cat(0),
+    _matmul(),
+    _in_dim1(in_dim1),
+    _in_dim2(in_dim2),
+    _output_dim(output_dim)
+{
+}
 
-        ~DeepBiaffine() {
-            delete[] _activation,
-            _ffn1, // or Dense
-            _ffn2,
-            _pairwise_weight,
-            _cat,
-            _matmul,
-            _in_dim1,
-            _in_dim2,
-            _output_dim;
-        }
+void DeepBiaffine::operator()(const StorageView& x1,
+                              const StorageView& x2,
+                              StorageView& output) const
+{
+  StorageView h1(x1.dtype(), x1.device()), h2(x2.dtype(), x2.device());
+  // TODO check the operator() for the class
+  (*_ffn1)(x1, h1);
+  (*_ffn2)(x2, h2);
 
-        void DeepBiaffine::operator()(
-            const StorageView& x1,
-            const StorageView& x2,
-            StorageView& output  
-        ) 
-        const{
-            StorageView h1(x1.dtype(), x1.device()), h2(x2.dtype(), x2.device());
-            // TODO check the operator() for the class
-            _ffn1(x1, h1);
-            _ffn2(x2, h2);
+  // make interactions
+  StorageView g1(h1.dtype(), h1.device()), g2(h2.dtype(), h2.device());
+  make_interactions(h1, g1);
+  make_interactions(h2, g2);
 
-            // make interactions
-            StorageView g1(h1.dtype(), h1.device()), g2(h2.dtype(), h2.device());
-            DeepBiaffine::make_interactions(h1, g1);
-            DeepBiaffine::make_interactions(h2, g2);
+  Shape g1_size(g1.shape()), g2_size(g2.shape());
 
-            Shape g1_size(g1.shape()), g2_size(g2.shape());
+  StorageView g1_w(g1.dtype(), g1.device());
+  g1 = g1.view(-1, g1_size[g1_size.size()]);
+  _matmul(g1, pairwise(), g1_w);
+  StorageView g2_t(g2.dtype(), g2.device());
+  ops::Transpose({1,2})(g2, g2_t);
 
-            StorageView g1_w(g1.dtype(), g1.device());
-            g1 = g1.view(-1, g1_size[g1_size.size()]);
-            _matmul(g1, this.pairwise(), g1_w);
-            StorageView g2_t(g2.dtype(), g2.device());
-            ops::Transpose({1,2})(g2, g2_t);
+  StorageView g1_w_g2(g2.dtype(), g2.device());
+  Shape g_w_shape{g1_size[0], g1_size[1]*_output_dim, g2_size[2]};
+  _matmul.bmm(g1_w.view(g_w_shape), g2_t, g1_w_g2);
+  ops::Transpose({2,3})(g1_w_g2.view(g_w_shape));
+  output = g1_w_g2;
+}
 
-            StorageView g1_w_g2(g2.dtype(), g2.device());
-            Shape g_w_shape{g1size[0], g1_size[1]*_output_dim, g2_size[2]};
-            _matmul.bmm(g1_w.view(g_w_shape), g2_t, g1_w_g2); 
-            ops::Transpose({2,3})(g1_w_g2.view(g_w_shape));
-            output = g1_w_g2;
-        }
+void DeepBiaffine::make_interactions(const StorageView& h,
+                                     StorageView& g) const
+{
+  // TODO what is the goal of this call?
+  // g(h.dtype(), h.device());
+  StorageView h_ones(h.dtype(), h.device());
+  h_ones.fill(1);
+  ops::Concat(h.shape().size() - 1)({&h, &h_ones}, g);
+}
 
-        void make_interactions(
-            const StorageView& h,
-            StorageView& g
-        ) 
-        const{
-            g(h.dtype(), h.device());
-            StorageView* h_ones(h.dtype(), h.device()).fill(1); // TODO write this with unique/shared ptr
-            std::vector<StorageView> to_concat{h, h_ones};
-            size_t ax = h.shape().size() - 1;
-            ops::Concat(ax)(to_concat,g);
-        }
+/**
+* This class is based on the trankit library in python
+*/
+PosdepDecoder::PosdepDecoder(const models::Model& model,
+                             const std::string& scope,
+                             const ops::ActivationType activation_type) :
+    Decoder(model.device()),
+    _upos_embeddings(model, scope),
+    _upos_ffn(build_optional_layer<Dense>(model, scope + "/_upos_ffn")),
+    _xpos_ffn(build_optional_layer<Dense>(model, scope + "/_xpos_ffn")),
+    _feats_ffn(build_optional_layer<Dense>(model, scope + "/_feats_ffn")),
+    _down_project(build_optional_layer<Dense>(model, scope + "/_down_project")),
+    _unlabeled(
+      model.get_variable(scope+"/unlabeled"),
+               scope+"/unlabeled",
+               activation_type),
+    _deprel(model.get_variable(scope+"/deprel"),
+            scope+'/deprel',
+            activation_type)
+{
+}
 
-        /**
-         * This class is based on the trankit library in python
-        */
-        void PosdepDecoder(
-                const models::Model& model, 
-                const std::string& scope,
-                const ops::ActivationType* activation_type = ops::ActivationType::ReLU
-            ): _upos_embeddings(),
-            _upos_ffn(
-                build_optional_layer<Dense>(model, scope + "/_upos_ffn")
-            ),
-            _xpos_ffn(
-                build_optional_layer<Dense>(model, scope + "/_xpos_ffn")
-            ),
-            _feats_ffn(
-                build_optional_layer<Dense>(model, scope + "/_feats_ffn")
-            ),
-            _down_project(
-                build_optional_layer<Dense>(model, scope + "/_down_project")
-            ),
-            _unlabeled(
-                model.get_variables(scope+'/unlabeled'),
-                scope+'/unlabeled',
-                activation_type
-            ),
-            _deprel(
-                model.get_variables(scope+'/deprel'),
-                scope+'/deprel',
-                activation_type
-            )
+void PosdepDecoder::operator()(
+  const StorageView& batch,
+  const StorageView& word_reprs,
+  const StorageView& cls_reprs,
+  StorageView& preds_output,
+  StorageView& deps_idxs_output
+) const
+{
+}
 
-            ~PosdepDecoder() {
-                delete[] _upos_embeddings,
-            _upos_ffn,
-            _xpos_ffn,
-            _feats_ffn,
-            _down_project,
-            _unlabeled,
-            _deprel
-            } 
+void PosdepDecoder::predict(
+  const StorageView batch,
+  const StorageView word_reprs,
+  const StorageView cls_reprs,
+  StorageView& predicted_upos,
+  StorageView& predicted_xpos,
+  StorageView& predicted_feats,
+  StorageView& dep_preds
+) const
+{
+  // upos
+  StorageView upos_score(word_reprs.dtype(), word_reprs.device());
+  predicted_upos.move_to(word_reprs.device(), word_reprs.dtype()
+                         );
+  (*_upos_ffn)(word_reprs, upos_score);
 
+}
 
-            void operator(
-                const StorageView& batch,
-                const StorageView& word_reprs,
-                const StorageView& cls_reprs,
-                StorageView& preds_output,
-                StorageView& deps_idxs_output
-            ) const{
+StorageView DeepBiaffine::pairwise() const
+{
+  return _pairwise_weight.view(-1, (_in_dim2+1)*_output_dim);
+}
 
-            }
-
-            void predict(
-                const StorageView batch, 
-                const StorageView word_reprs, 
-                const StorageView cls_reprs,
-                StorageView& predicted_upos, 
-                StorageView& predicted_xpos, 
-                StorageView& predicted_feats, 
-                StorageView& dep_preds
-            ) const{
-                // upos
-                StorageView upos_score(word_reprs.dtype(), word_reprs.device()), predicted_upos(word_reprs.dtype(), word_reprs.device());
-                _upos_ffn(word_reprs, upos_score);
-
-            }
-
-    } // namespace layers
-} // namespace ctranslate2
+} // namespace ctranslate2::layers
